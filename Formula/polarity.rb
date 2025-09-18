696b6abba28a70750a17ff65235145fe2ff366ed
Alex <alexu8007@gmail.com>
2025-09-17 23:04:33 -0400

    Update polarity.rb

--- Formula/polarity.rb
+++ Formula/polarity.rb
@@ -1,8 +1,8 @@
 class Polarity < Formula
   desc "CLI tool for managing stacked pull requests with AI-powered features"
   homepage "https://polarity.cc"
-  url "https://github.com/Polarityinc/polarity-binaries/releases/download/v0.0.6/polarity-macos-v0.0.6.tar.gz"
-  sha256 "8254232db7428b56aba39dee88e55f2ee3ced0f42e5125c5139f88efd447e343"
+  url "https://github.com/Polarityinc/polarity-binaries/releases/download/v0.0.8/polarity-macos-v0.0.8.tar.gz"
+  sha256 "7d6b0ccd554e8f83ec4bd9439f6c803110af6c4592f80c134538642ae10e3d05"
   license "MIT"
 
   depends_on "git"
